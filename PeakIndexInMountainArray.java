911fae53d8f5433225eb6fd64c0e656beab0cad7
Dhruv B <42473590+strygwyr69@users.noreply.github.com>
2022-11-14 16:52:56 +0530

    Leetcode solution to problems 852 & 162.

---
+++ PeakIndexInMountainArray.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+//https://leetcode.com/problems/find-peak-element/
+
+public class PeakIndexInMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start)/2;
+
+            if (arr[mid] > arr[mid+1]) {
+                end = mid;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
