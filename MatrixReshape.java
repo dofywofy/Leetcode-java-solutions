a91e6aa821d457ae0ef22419647fbf86e58e352d
Dhruv B <42473590+strygwyr69@users.noreply.github.com>
2022-11-24 17:42:27 +0530

    Adding more solutions...
    Leetcode solution number 566.

---
+++ MatrixReshape.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/reshape-the-matrix/
+
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+
+        if((rows * cols) != (r * c)) return mat;
+
+        int[][] newMat = new int[r][c];
+        int newRow = 0;
+        int newCol = 0;
+
+        
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < cols; j++)
+            {
+                newMat[newRow][newCol] = mat[i][j];
+                newCol++;
+                
+                if(newCol == c)
+                {
+                    newCol = 0;
+                    newRow++;
+                }
+            }
+        }
+
+        return newMat;
+    }
+}
