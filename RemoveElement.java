a78a67b3acad87c1353114a8f1f4f1d9505fed66
Dhruv B <42473590+strygwyr69@users.noreply.github.com>
2022-12-02 21:15:06 +0530

    Leetcode solution number 27.

---
+++ RemoveElement.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/remove-element/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int newSize = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[newSize] = nums[i];
+                newSize++;
+            }
+        }
+        return newSize;
+    }
+}
