2c5d1f4f4fde724cf19dcb547b20e89714b3f5c6
Dhruv B <42473590+strygwyr69@users.noreply.github.com>
2022-12-05 19:23:02 +0530

    Leetcode solution number 69.

---
+++ MySqrt.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/sqrtx/
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+        int start = 1, end = x, ans = 0;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (mid <= x / mid) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
